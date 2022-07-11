4e471bf303326ee8478b81a09051d2e3d1b64000
mig <michaelleojacob@gmail.com>
2022-07-10 22:48:15 -0700

    broke up editProfile into a bunch of smaller components and put them into their own files.
    previously, all the child components were all in editProfile.js

--- src/app/components/editProfile.js
+++ src/app/components/editProfile.js
@@ -1,14 +1,12 @@
 import DevNav from './devNav';
 import { useSelector, useDispatch } from 'react-redux';
-import { checkLocalUser, updateDynamic } from '../redux-store/userState';
-import {
-  deleteImgFromUserDocAndBucket,
-  updateUser,
-  addPhotoToBucketAndDocs,
-} from '../firebase-utils/firestoreUser';
+import { checkLocalUser } from '../redux-store/userState';
 import { useState, useEffect } from 'react';
 import { getDownloadURL, ref, listAll } from 'firebase/storage';
 import { storage } from '../firebase-utils/firebasePhotos';
+import UserPhotoCards from './editProfile/userPhotoCards';
+import UploadImage from './editProfile/uploadImage';
+import FormComp from './editProfile/formComp';
 
 const EditProfile = () => {
   // redux getter and setter
@@ -69,80 +67,6 @@ const EditProfile = () => {
         userPhotos={userPhotos}
         triggerUseEffect={triggerUseEffect}
       />
-      {/* <button onClick={() => console.log(userPhotos)}>check userPhotos</button> */}
-    </div>
-  );
-};
-
-const FormComp = ({ label, state, dispatch }) => {
-  const handleDynamic = (e) => {
-    dispatch(updateDynamic({ label, value: e.target.value }));
-  };
-  const updateDb = async () => {
-    await updateUser(label, state);
-  };
-  return (
-    <form className='m-1 p-1' onSubmit={(e) => e.preventDefault()}>
-      <label>{label}</label>
-      <input
-        onBlur={() => updateDb(label, state)}
-        onChange={(e) => handleDynamic(e)}
-        value={state[label]}
-        className='border-2 border-slate-900 m-1 p-1 rounded'></input>
-    </form>
-  );
-};
-
-const UploadImage = ({ user, triggerUseEffect }) => {
-  const handleChange = async (e) => {
-    const res = await addPhotoToBucketAndDocs(user.uid, e.target.files[0]);
-    if (res) triggerUseEffect();
-  };
-  return (
-    <input
-      onChange={handleChange}
-      className='m-1 p-1'
-      type={'file'}
-      accept='image/pngm image/jpeg'></input>
-  );
-};
-
-const UserPhotoCards = ({ user, userPhotos, triggerUseEffect }) => {
-  const [photosArr, setPhotosArr] = useState([]);
-
-  const remomveFromPhotosArr = (index) =>
-    setPhotosArr(photosArr.filter((item, itemIndex) => itemIndex !== index));
-
-  const handleDelete = async (index, locationURL) => {
-    const res = await deleteImgFromUserDocAndBucket(user.uid, locationURL);
-    if (res) remomveFromPhotosArr(index);
-  };
-
-  useEffect(() => {
-    setPhotosArr([...userPhotos]);
-  }, [userPhotos]);
-
-  return (
-    <div className='editprofile-card-area'>
-      {userPhotos.length
-        ? photosArr.map((photo, index) => {
-            return (
-              <div
-                className='editprofile-img-cards'
-                key={`${photo.imgLocation}-${index}`}>
-                <img
-                  className='editprofile-photo'
-                  src={photo.downloadURL}
-                  alt={`${photo}-${index}`}></img>
-                <button
-                  onClick={() => handleDelete(index, photo.imgLocation)}
-                  className='bg-red-500 hover:bg-red-700 text-white py-1 px-1 rounded m-1 p-1 border border-red-700 rounded'>
-                  delete
-                </button>
-              </div>
-            );
-          })
-        : null}
     </div>
   );
 };
---
+++ src/app/components/editProfile/userPhotoCards.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { deleteImgFromUserDocAndBucket } from '../../firebase-utils/firestoreUser';
+
+const UserPhotoCards = ({ user, userPhotos, triggerUseEffect }) => {
+  const [photosArr, setPhotosArr] = useState([]);
+
+  const remomveFromPhotosArr = (index) =>
+    setPhotosArr(photosArr.filter((item, itemIndex) => itemIndex !== index));
+
+  const handleDelete = async (index, locationURL) => {
+    const res = await deleteImgFromUserDocAndBucket(user.uid, locationURL);
+    if (res) remomveFromPhotosArr(index);
+  };
+
+  useEffect(() => {
+    setPhotosArr([...userPhotos]);
+  }, [userPhotos]);
+
+  return (
+    <div className='editprofile-card-area'>
+      {userPhotos.length
+        ? photosArr.map((photo, index) => {
+            return (
+              <div
+                className='editprofile-img-cards'
+                key={`${photo.imgLocation}-${index}`}>
+                <img
+                  className='editprofile-photo'
+                  src={photo.downloadURL}
+                  alt={`${photo}-${index}`}></img>
+                <button
+                  onClick={() => handleDelete(index, photo.imgLocation)}
+                  className='bg-red-500 hover:bg-red-700 text-white py-1 px-1 rounded m-1 p-1 border border-red-700 rounded'>
+                  delete
+                </button>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
+};
+
+export default UserPhotoCards;
