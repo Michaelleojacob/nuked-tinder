8adf76ceb3cf6deb846da94d581a29faff80af5e
mig <michaelleojacob@gmail.com>
2022-07-24 20:14:14 -0700

    trying auth.uid !== null instead of request.auth

--- src/app/firebase-utils/firebase-chatrooms.js
+++ src/app/firebase-utils/firebase-chatrooms.js
@@ -4,8 +4,8 @@ import { db } from './firebase';
 export const createChatRoom = async (uids) => {
   try {
     console.log(uids);
-    const newChatRoom = `${uids[0]}${uids[1]}`;
-    await setDoc(doc(db, 'rooms', newChatRoom), {});
+    // const newChatRoom = `${uids[0]}${uids[1]}`;
+    await setDoc(doc(db, 'rooms'), {});
   } catch (e) {
     console.error(e);
   }
