b8d50fc686ffaceb0fc86ccbd086726d52ad72c9
mig <michaelleojacob@gmail.com>
2022-07-24 22:10:35 -0700

    added chat rooms or 'rooms' in the db. Sends a intro message when two people match.

--- src/app/firebase-utils/firebase-chatrooms.js
+++ src/app/firebase-utils/firebase-chatrooms.js
@@ -1,11 +1,26 @@
-import { setDoc, doc } from 'firebase/firestore';
+import {
+  setDoc,
+  doc,
+  serverTimestamp,
+  addDoc,
+  collection,
+} from 'firebase/firestore';
 import { db } from './firebase';
 
 export const createChatRoom = async (uids) => {
   try {
-    console.log(uids);
-    // const newChatRoom = `${uids[0]}${uids[1]}`;
-    await setDoc(doc(db, 'rooms'), {});
+    const name = `${uids[0]}-${uids[1]}`;
+    await setDoc(doc(db, 'rooms', name), {
+      name,
+      chatUsers: [...uids],
+    });
+    const docRef = doc(db, 'rooms', name);
+    const colRef = collection(docRef, 'messages');
+    await addDoc(colRef, {
+      msg: 'You both matched! Be the first to say something.',
+      from: 'tinder-clone',
+      timestamp: serverTimestamp(),
+    });
   } catch (e) {
     console.error(e);
   }
